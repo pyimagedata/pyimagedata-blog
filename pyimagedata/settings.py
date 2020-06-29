e09fda5cdd06a3d240c295df11bea24c112914eb
= <=>
2020-06-29 22:18:16 +0300

    debug value changed

--- pyimagedata/settings.py
+++ pyimagedata/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = (os.environ.get("DEBUG_VALUE") == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['pyimagedata.herokuapp.com']
 
