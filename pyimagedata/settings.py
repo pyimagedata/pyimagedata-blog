7170e48cdf43d364123b1038f80b5c66078edfef
= <=>
2020-06-29 20:55:04 +0300

    debug value created

--- pyimagedata/settings.py
+++ pyimagedata/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = os.environ.get("DEBUG_VALUE")
+#DEBUG = True
+DEBUG = (os.environ.get("DEBUG_VALUE") == 'True')
 
 ALLOWED_HOSTS = ['pyimagedata.herokuapp.com']
 
